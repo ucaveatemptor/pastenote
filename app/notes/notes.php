5b8b493a3aa5f6040a13283b2cf0496419ac95bc
uphethy <normagaok@gmail.com>
2024-11-02 21:46:52 +0300

    sep

--- app/notes/notes.php
+++ app/notes/notes.php
@@ -11,26 +11,7 @@
     <div>Sobriquet: <?=$_COOKIE['showUsername']?>.</div>
     <button><a href="../notes/createNote.php">Create note</a></button>
     <?php
-        require_once '../../sql/db.php';
-        $user = $_COOKIE['showUsername'];
-        $sqlUID = 'SELECT id FROM users WHERE name = ?';
-        $queryUID = $pdo->prepare($sqlUID);
-        $queryUID->execute([$user]);
-        $row = $queryUID->fetch();
-        $userId = $row[0];
-        $sql = 'SELECT id, label, text, date FROM notes WHERE userId = ?';
-        $query = $pdo->prepare($sql);
-        $query->execute([$userId]);
-        $notes = $query->fetchAll(PDO::FETCH_OBJ);
-
-        foreach($notes as $note) {
-            echo '
-            <div class="note">
-                <p style>'. $note->label .'<br></p>
-                <a>'. $note->text.'<br><br></a>
-                <a class="redtext"> Дата создания: '. $note->date .'</a>
-            </div>';
-        }
+        require_once "../src/notes.php"
     ?>
 </body>
 </html>
---
+++ app/src/notes.php
@@ -0,0 +1,21 @@
+<?php
+    require_once '../../sql/db.php';
+    $user = $_COOKIE['showUsername'];
+    $sqlUID = 'SELECT id FROM users WHERE name = ?';
+    $queryUID = $pdo->prepare($sqlUID);
+    $queryUID->execute([$user]);
+    $row = $queryUID->fetch();
+    $userId = $row[0];
+    $sql = 'SELECT id, label, text, date FROM notes WHERE userId = ?';
+    $query = $pdo->prepare($sql);
+    $query->execute([$userId]);
+    $notes = $query->fetchAll(PDO::FETCH_OBJ);
+
+    foreach($notes as $note) {
+        echo '
+        <div class="note">
+            <p style>'. $note->label .'<br></p>
+            <a>'. $note->text.'<br><br></a>
+            <a class="redtext"> Дата создания: '. $note->date .'</a>
+        </div>';
+    }
