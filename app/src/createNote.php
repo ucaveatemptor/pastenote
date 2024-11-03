8d44a5cc8bd50fb033bf6e30a9159ee68289d5c0
uphethy <normagaok@gmail.com>
2024-11-03 15:40:25 +0300

    label/text length restrictions

--- app/src/createNote.php
+++ app/src/createNote.php
@@ -5,9 +5,15 @@
     if (strlen($label) == 0) {
         echo "lable length = 0 amongus<br>";
         exit;
+    } else if(strlen($label) > 200) {
+        echo "Max length of label: 200, you have: " . strlen($label);
+        exit;
     }
     if (strlen($text) == 0) {
         echo "text length = 0 not acceptable<br>";
+        exit;
+    } else if(strlen($text) > 30000) {
+        echo "Max length of text: 30000, you have: " . strlen($text);
         exit;
     }
     $user = $_COOKIE['showUsername'];
